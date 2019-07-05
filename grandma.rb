1ab21ec48b4b450b25d9efc359c7b40653477336
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-05 02:47:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-  puts  "NO, NOT SINCE 1938"
+  return  "NO, NOT SINCE 1938"
 end
 end
